d681e040b33c0ca03653a749abf07bfdebeb4e23
Yesh Ramesh <yesh.ramesh@workday.com>
2022-12-02 13:45:38 -0800

    Day 2 complete

---
+++ day02/part1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	x := 0
+	y := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, " ")
+
+		direction := split[0]
+		distance, _ := strconv.Atoi(split[1])
+
+		if direction == "forward" {
+			x += distance
+		} else if direction == "up" {
+			y -= distance
+			if y < 0 {
+				y = 0
+			}
+		} else if direction == "down" {
+			y += distance
+		}
+	}
+
+	fmt.Println(x * y)
+
+}
---
+++ day02/part2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	x := 0
+	y := 0
+	aim := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, " ")
+
+		direction := split[0]
+		units, _ := strconv.Atoi(split[1])
+
+		if direction == "up" {
+			aim -= units
+		} else if direction == "down" {
+			aim += units
+		} else if direction == "forward" {
+			x += units
+			y += units * aim
+			if y < 0 {
+				y = 0
+			}
+		}
+	}
+
+	fmt.Println(x * y)
+}
